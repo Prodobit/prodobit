7ee9557d503f2a6b804e0aa93bcd5178cdcbf263
Hasan YILDIZ <hasan.yildizz@gmail.com>
2025-08-28 17:37:10 +0300

    Refactor code structure for improved readability and maintainability

--- packages/sdk/src/framework/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './api-methods';
-export * from './query-keys';
-export * from './cache-utils';
-export * from './auth-state';
-//# sourceMappingURL=index.d.ts.map
--- packages/sdk/src/singleton.d.ts
+++
@@ -1,5 +0,0 @@
-import { ProdobitClient } from './client';
-import type { ProdobitClientConfig } from './types';
-export declare function getProdobitClientSingleton(config: ProdobitClientConfig): ProdobitClient;
-export declare function clearProdobitClientSingleton(): void;
-//# sourceMappingURL=singleton.d.ts.map
